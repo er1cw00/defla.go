1d565bea905b0d705b8e4db80f9f553e2aadc244
er1cw00 <wadahana@qq.com>
2023-04-11 23:03:31 +0800

    misc: refactor nage.go -> session.go

---
+++ app/session.go
@@ -0,0 +1,106 @@
+package app
+
+import (
+	"crypto/md5"
+	"io"
+	"os"
+
+	cs "github.com/er1cw00/btx.go/asm/cs"
+	ks "github.com/er1cw00/btx.go/asm/ks"
+	logger "github.com/er1cw00/btx.go/base/logger"
+	emu "github.com/er1cw00/btx.go/emu"
+	android "github.com/er1cw00/btx.go/emu/android"
+
+	core "github.com/er1cw00/defla.go/core"
+	//defla "github.com/er1cw00/defla.go/core/defla"
+	//egn "github.com/er1cw00/btx.go/engine"
+)
+
+type Session struct {
+	Id       string
+	Capstone *cs.Capstone
+	Keystone *ks.Keystone
+	Emulator emu.Emulator
+	Module   emu.Module
+}
+
+func NewSession() (*Session, error) {
+	var err error = nil
+	var e emu.Emulator = nil
+	var fs emu.FileSystem = nil
+
+	var capstone *cs.Capstone = nil
+	var keystone *ks.Keystone = nil
+
+	if capstone, err = cs.NewCapstone(cs.CS_ARCH_ARM64, cs.CS_MODE_ARM); err != nil {
+		logger.Errorf("create capstone for arm64 fail, error: %v", err)
+		return nil, err
+	}
+	if keystone, err = ks.NewKeystone(ks.KS_ARCH_ARM64, ks.KS_MODE_LITTLE_ENDIAN); err != nil {
+		logger.Errorf("create keystone for arm64 fail, error: %v", err)
+		return nil, err
+	}
+
+	if e, err = android.NewAndroidEmulator("xloader", emu.ARCH_ARM64); err != nil {
+		logger.Errorf("create android arm64 emulator fail, error: %v", err)
+		return nil, err
+	}
+
+	fs = e.GetFileSystem()
+	fs.SetRootfsPath(core.Config.RootfsPath)
+
+	session := &Session{
+		Id:       "",
+		Emulator: e,
+		Capstone: capstone,
+		Keystone: keystone,
+		Module:   nil,
+	}
+	return session, nil
+
+}
+
+func (session *Session) Close() {
+	if session.Keystone != nil {
+		session.Keystone.Close()
+		session.Keystone = nil
+	}
+	if session.Capstone != nil {
+		session.Capstone.Close()
+		session.Capstone = nil
+	}
+	if session.Emulator != nil {
+		//session.emulator.Close()
+		session.Emulator = nil
+	}
+}
+
+func md5Sum(libPath string) (string, error) {
+	f, err := os.Open("file.txt")
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	h := md5.New()
+	if _, err := io.Copy(h, f); err != nil {
+
+		return "", err
+	}
+	return string(h.Sum(nil)), nil
+}
+func (session *Session) Load(libPath string) error {
+	var err error = nil
+	loader := session.Emulator.GetLoader()
+	loader.SetInitFuncFilter(func(loader emu.Loader, module emu.Module) bool {
+		return false
+	})
+	if session.Module, err = loader.LoadLibrary(libPath); err != nil {
+		logger.Errorf("load [%s] fail, error: %v", libPath, err)
+		return err
+	}
+	if session.Id, err = md5Sum(libPath); err != nil {
+		logger.Errorf("md5sum for [%s] fail, error: %v", libPath, err)
+	}
+	return err
+}
--- defla_cli.go
+++ defla_cli.go
@@ -6,13 +6,15 @@ import (
 	"os"
 
 	base "github.com/er1cw00/btx.go/base"
+	logger "github.com/er1cw00/btx.go/base/logger"
 	egn "github.com/er1cw00/btx.go/engine"
 
 	app "github.com/er1cw00/defla.go/app"
+	defla "github.com/er1cw00/defla.go/core/defla"
 )
 
 var rootfsPath string = ""
-var xloaderPath string = ""
+var modulePath string = ""
 var funcPath string = ""
 var help bool = false
 
@@ -27,9 +29,45 @@ func usage() {
 func init() {
 	flag.BoolVar(&help, "h", false, "print help")
 	flag.StringVar(&rootfsPath, "r", "", "rootfs path")
-	flag.StringVar(&xloaderPath, "i", "", "library path")
+	flag.StringVar(&modulePath, "i", "", "library path")
 	flag.StringVar(&funcPath, "f", "", "func list json path")
 	flag.Usage = usage
+}
+
+func Example(rootfsPath, modulePath, funcPath string) error {
+	var err error = nil
+	var session *app.Session = nil
+	var funcs []app.FuncModel = nil
+	if funcs, err = app.UnmarshalFunctions(funcPath); err != nil {
+		logger.Errorf("load defla func list fail, error: %v", err)
+		return err
+	}
+
+	if session, err = app.NewSession(); err != nil {
+		logger.Errorf("create session fail, error: %v", err)
+		return err
+	}
+	if err = session.Load(modulePath); err != nil {
+		logger.Errorf("load module [%s] fail, error: %v", modulePath, err)
+		return err
+	}
+	m := session.Module
+	logger.Debugf("load %s at 0x%x", m.GetName(), m.GetMapBase())
+
+	for i, fn := range funcs {
+		logger.Debugf("func(%d), name(%s), start(0x%0x), end(0x%x)", i, fn.Name, fn.Start, fn.End)
+	}
+
+	fn := funcs[12]
+	bbList, err := defla.NewBBList(session.Capstone, fn.Name, m.GetLoadBase()+fn.Start, fn.Start, fn.End)
+	if err != nil {
+		logger.Fatalf("parse function to basic block fail, err: %v", err)
+	}
+	fmt.Printf("%s\n", bbList.String())
+	//_ = defla.ParseFunction(naga.capstone, fn.Name, m.GetLoadBase()+fn.Start, fn.Start, fn.End)
+
+	session.Close()
+	return nil
 }
 
 func main() {
@@ -43,7 +81,7 @@ func main() {
 	}
 	egn.Init()
 
-	app.Run(rootfsPath, xloaderPath, funcPath)
+	Example(rootfsPath, modulePath, funcPath)
 
 	egn.Close()
 	base.Stop()
